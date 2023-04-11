4725e5b1b66aa55c82ca3216695fab8b3f4d5401
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-04-11 20:08:35 +0300

    fix master

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import './App.css'
-
-
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-    let msg = 'hello okten';
-
-    return (
-        <div>
-            <h1>{msg}</h1>
-
-
-        </div>
-
-    );
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
