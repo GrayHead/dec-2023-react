151aa68cddd8028266d81b3950c48555cdb34aef
GrayHead <ss.zhuravlov@gmail.com>
2023-04-12 19:40:52 +0300

    lesson 3 branch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import './App.css'
-import Users from "./components/users/Users";
 
 
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
-
-function App() {
-
-    let msg = 'hello okten';
-
+const App = () => {
     return (
         <div>
-            <h1>{msg}</h1>
-
-            <Users xxx={users}/>
-            <hr/>
 
 
         </div>
