9423bd967c6eabe8d1768e113d95b925b9bb7208
GrayHead <ss.zhuravlov@gmail.com>
2023-04-27 20:41:12 +0300

    redux base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import {useDispatch, useSelector} from "react-redux";
+
 const App = () => {
+
+    let store = useSelector(state => state);
+    let dispatch = useDispatch();
+
+    const increment = () => {
+        dispatch({type: 'INC', payload: 2});
+
+    };
+    const decrement = () => {
+        dispatch({type: 'DEC', payload: 3});
+    };
+    const res = () => {
+        dispatch({type: 'RESET'});
+    };
     return (
         <div>
-            App
+
+            <h1>{store}</h1>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <button onClick={res}>reset</button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+const baseReducer = (state = 0, action) => {
+    switch (action.type) {
+        case "INC":
+            return state += action.payload;
+        case "DEC":
+            return state -= action.payload;
+        case "RESET":
+            return 0;
+        default:
+            return state;
+
+
+    }
+
+}
+
+let store = createStore(baseReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
